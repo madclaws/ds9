e31c2b6074657bac6d50bfa816e59c0cd243aad3
madclaws <pavanan.anandu63@gmail.com>
2025-02-22 21:51:41 +0530

    feat: basic file receive and download

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use anyhow::Result;
 use iroh::{protocol::Router, Endpoint};
 use iroh_blobs::{
-    net_protocol::Blobs, rpc::client::blobs::WrapOption, ticket::BlobTicket, util::SetTagOption,
+    net_protocol::Blobs,
+    rpc::client::blobs::WrapOption,
+    store::{ExportFormat, ExportMode},
+    ticket::BlobTicket,
+    util::SetTagOption,
 };
 use std::path::PathBuf;
 #[tokio::main]
@@ -24,29 +28,66 @@ async fn main() -> Result<()> {
     match args_ref.as_slice() {
         ["send", filename] => {
             let path: PathBuf = filename.parse()?;
-            let path_clone = path.clone();
-            let abs_path = std::path::absolute(path)?;
+            let abs_path = std::path::absolute(path.clone())?;
 
             println!("Blaking it");
 
             let blob = client
-                .add_from_path(abs_path, true, SetTagOption::Auto, WrapOption::NoWrap)
+                .add_from_path(
+                    abs_path.clone(),
+                    true,
+                    SetTagOption::Auto,
+                    WrapOption::NoWrap,
+                )
                 .await?
                 .finish()
                 .await?;
 
             let node_id = router.endpoint().node_id();
             let ticket = BlobTicket::new(node_id.into(), blob.hash, blob.format)?;
+
             println!("File hashed. Fetch this file by running:");
-            println!("cargo run receive {:?} {:?}", ticket, path_clone);
+            println!(
+                "cargo run receive {:?} {:?}",
+                ticket.to_string(),
+                path.clone()
+            );
             tokio::signal::ctrl_c().await?;
         }
-        ["receive", _ticket, filename] => {
-            todo!()
+        ["receive", ticket, filename] => {
+            let abs_path: PathBuf = std::path::absolute(filename.parse::<PathBuf>()?.clone())?;
+            let ticket: BlobTicket = ticket.parse()?;
+
+            println!("Starting download");
+
+            client
+                .download(ticket.hash(), ticket.node_addr().clone())
+                .await?
+                .finish()
+                .await?;
+
+            println!("download finished");
+
+            println!("copying to dest");
+
+            client
+                .export(
+                    ticket.hash(),
+                    abs_path,
+                    ExportFormat::Blob,
+                    ExportMode::Copy,
+                )
+                .await?
+                .finish()
+                .await?;
+
+            println!("Finished download.");
         }
         _ => {
             println!("Commands are wrong")
         }
     }
+
+    println!("Gracefull shutdown");
     Ok(())
 }
